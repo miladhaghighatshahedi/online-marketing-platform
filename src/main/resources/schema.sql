48e5f6e823ce9d05ebcf86591eb6e073350fd72b
Milad <miladhaghighatshahedi@gmail.com>
2025-08-25 20:44:58 +0330

    commit: sql schema updated
    sql schema updated with:
    
    1_credential (here the owner which has lots of work and is a blueprint for now)
    2_Profile which has a oneToOne relationship with owner
    3_products which has a ManyToOne relationship with credential (many products can belong to a single owner(credential)) also duplicate products allowed here
    
    4_catalogs and categories which has a oneToMany relationship with the categories which means a catalog can have many categories
    
    5_product_category to hold product and category relationship

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,26 +1,70 @@
+create table if not exists credentials
+(
+    id       uuid unique primary key not null,
+    version  integer                 not null,
+    username varchar(100) unique     not null,
+    password varchar(100)            not null,
+    enabled  boolean                 not null
+);
+
 create table if not exists profiles
 (
-    id uuid unique primary key not null,
-    version integer not null ,
-    name varchar(100) not null ,
-    about varchar(250),
-    profile_type varchar(50) not null,
-    profile_status varchar(50) not null,
-    activation_date timestamp,
-    credential uuid unique
+    id              uuid unique primary key not null,
+    version         integer                 not null,
+    name            varchar(100) unique     not null,
+    about           text,
+    profile_type    varchar(50)             not null,
+    profile_status  varchar(50)             not null,
+    activation_date timestamp               not null,
+    credential      uuid unique references credentials (id)
 );
+
+create index if not exists index_profiles_credential on products (credential);
 
 create table if not exists products
 (
-    id uuid primary key not null,
-    version integer not null ,
-    name varchar(100) not null,
-    description varchar(250),
-    insert_date timestamp not null,
-    update_date timestamp,
-    price NUMERIC(15,2) not null ,
-    product_status varchar(50) not null ,
-    credential uuid
+    id             uuid primary key not null,
+    version        integer          not null,
+    name           varchar(100)     not null,
+    description    text,
+    inserted_at    timestamp        not null,
+    updated_at     timestamp,
+    price          numeric(15, 2)   not null,
+    product_status varchar(50)      not null,
+    credential     uuid references credentials (id)
 );
+
+create index if not exists index_products_credential on products (credential);
+
+create table if not exists product_category
+(
+    product  uuid not null references products (id),
+    category uuid not null references categories (id),
+    primary key (product, category)
+);
+
+
+create table if not exists catalogs
+(
+    id          uuid primary key    not null,
+    version     integer             not null,
+    name        varchar(150)        not null,
+    description text,
+    slug        varchar(160) unique not null
+);
+
+create table if not exists categories
+(
+    id              uuid primary key    not null,
+    version         integer             not null,
+    name            VARCHAR(150)        not null,
+    description     text,
+    created_at      timestamp           not null,
+    updated_at      timestamp,
+    category_status varchar(50)         not null,
+    slug            varchar(160) unique not null,
+    catalogs        uuid                not null REFERENCES catalogs (id)
+);
+
 
 
