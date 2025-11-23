d58842f83c57c417ed7f06714fcf6e452a3e3425
Milad Haghighat Shahedi <miladhaghighatshahedi@gmail.com>
2025-11-23 23:46:40 +0330

    commit (jackson converters): refactored JaksonConfig dateTime formatted
    JaksonConfig.class which is a config class refactored:
    
    1_A JavaTimeModule object created.
    2_A new DateTimeFormatter object with a custom pattern created.
    3_A deserializer for custom DateTimeFormatter added to JavaTimeModule.
    4_A serializer for custom DateTimeFormatter added to JavaTimeModule.
    6_The javaTimeModule registered.
    7_WRITE_DATES_AS_TIMESTAMPS disabled.
    8_A SimpleModule object called bigDecModule created.
    9_ A BigDecimalPlainDeserializer class defined.
    10_The BigDecimalPlainDeserializer added to SimpleModule.
    11_SimpleModule registered.
    12_null values not included anymore.
    13_Naming strategy set to SNAKE_CASE
    14_added for backward compatibility {
    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
    }

---
+++ src/main/java/com/mhs/onlinemarketingplatform/advertisement/config/JaksonConfig.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2025-2026 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mhs.onlinemarketingplatform.advertisement.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Milad Haghighat Shahedi
+ */
+@Configuration
+public class JaksonConfig {
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+
+		JavaTimeModule javaTimeModule = new JavaTimeModule();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
+		javaTimeModule.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
+		javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+		mapper.registerModule(javaTimeModule);
+		mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+		SimpleModule bigDecModule = new SimpleModule();
+		bigDecModule.addDeserializer(BigDecimal.class, new BigDecimalPlainDeserializer());
+		mapper.registerModule(bigDecModule);
+
+		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+		mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES,false);
+
+		return mapper;
+	}
+
+}
+
+class BigDecimalPlainDeserializer extends JsonDeserializer<BigDecimal> {
+	@Override
+	public BigDecimal deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException {
+		String value = parser.getText();
+
+		if (value == null || value.isBlank()) {
+			return null;
+		}
+
+		return new BigDecimal(parser.getText());
+	}
+}
