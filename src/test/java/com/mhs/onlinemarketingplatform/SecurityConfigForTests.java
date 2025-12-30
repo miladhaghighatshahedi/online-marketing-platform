1121d2544f6ae86546d36b577376471b35ceda9b
Milad Haghighat Shahedi <miladhaghighatshahedi@gmail.com>
2025-12-30 22:35:25 +0330

    test (authentication module): introduce SecurityConfigForTests in the test layer
    Introduced SecurityConfigForTests in the test layer to replace Security Config in the main application.

---
+++ src/test/java/com/mhs/onlinemarketingplatform/SecurityConfigForTests.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025-2026 the original author.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.mhs.onlinemarketingplatform;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+/**
+ * @author Milad Haghighat Shahedi
+ */
+@TestConfiguration
+@EnableWebSecurity
+public class SecurityConfigForTests {
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		return http
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.build();
+	}
+
+}
